eb8d29aa2e56e12b9b26bcfbd89cf74b9e9139e3
DoanGiaHuy0301 <huydg0301@gmail.com>
2023-09-19 14:51:12 +0700

    update

--- SaleAppV1/target/generated-sources/annotations/com/dgh/pojo/ThongTinChiTietKhachHangDatTiec_.java
+++ SaleAppV1/target/generated-sources/annotations/com/dgh/pojo/ThongTinChiTietKhachHangDatTiec_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.SetAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-09-08T21:08:00")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-09-14T23:07:10")
 @StaticMetamodel(ThongTinChiTietKhachHangDatTiec.class)
 public class ThongTinChiTietKhachHangDatTiec_ { 
 
